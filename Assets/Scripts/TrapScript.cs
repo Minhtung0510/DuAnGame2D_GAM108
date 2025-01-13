9532dc7e67fee0483dcc560dd247100cb89114d3
Minhtung0510 <169137996+Minhtung0510@users.noreply.github.com>
2025-01-13 20:34:30 +0700

    1
    1

---
+++ Assets/Scripts/TrapScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TrapScript : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("player mất máu");
+        }
+    }
+}
