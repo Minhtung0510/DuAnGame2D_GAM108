527196283c4e433f1e2901da71adae8cd9d7acfb
ptrieu98 <snkuntroll25@gmail.com>
2025-02-24 01:50:29 +0700

    1
    1

--- Assets/Scripts/Player/FragmentCollector.cs
+++ Assets/Scripts/Player/FragmentCollector.cs
@@ -3,21 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class FragmentCollector : MonoBehaviour
 {
-    private int fragmentCount = 0;
-    public int requiredFragments = 3;
-    public string bossSceneName = "Boss"; // Đổi tên scene boss của bạn
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.CompareTag("Fragment"))
     {
-        if (other.CompareTag("Fragment")) // Đặt tag "Fragment" cho mảnh vỡ
-        {
-            Destroy(other.gameObject); // Xóa mảnh vỡ
-            fragmentCount++;
-
-            if (fragmentCount >= requiredFragments)
-            {
-                SceneManager.LoadScene(bossSceneName); // Dịch chuyển đến boss scene
-            }
-        }
+        Destroy(other.gameObject);
+        GameManager.Instance.CollectFragment();
     }
+}
+
 }
---
+++ Assets/Scripts/Player/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int fragmentCount = 0;
+    public string bossSceneName = "BossScene";
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CollectFragment()
+    {
+        fragmentCount++;
+
+        if (fragmentCount >= 3)
+        {
+            SceneManager.LoadScene(bossSceneName);
+        }
+    }
+}
